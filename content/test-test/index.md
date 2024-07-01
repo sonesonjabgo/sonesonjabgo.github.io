cc898a114a051d79f4af8d6b3e2989066565256c
sonesonjabgo <thswogud02@gmail.com>
2024-07-01 14:33:28 +0900

    test post

---
+++ content/test-test/index.md
@@ -0,0 +1,12 @@
+---
+emoji: 🧢
+title: Getting Started
+date: '2024-07-01 11:00:00'
+author: 손재형
+tags: tutorial
+categories: tutorial
+---
+
+# TEST
+
+- this is test
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -18,9 +18,9 @@ module.exports = {
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/sonesonjabgo`, // `https://github.com/zoomKoding`,
+      linkedIn: `https://www.linkedin.com/in/jaehyung-a225b3255/`, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: `thswogud02@gmail.com`, // `zoomkoding@gmail.com`,
     },
   },
 
